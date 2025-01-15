1705844ab7eb73fd3a1319a266236dce4e228732
krila515 <krila515@student.liu.se>
2025-01-15 11:07:03 +0100

    Starting on fetching news

---
+++ scraping/news.py
@@ -0,0 +1,15 @@
+# Should give notification if stock in question has any news 
+# on https://allaaktier.se/nyheter, since this is primarily
+# for analyzing swedish stocks.
+# Part of the fundamental analyzsis when considering purchasing a stock
+
+
+def fetch_news():
+
+    
+
+def notify_news():
+
+    pass
+
+
